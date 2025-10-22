747c6002e11ff041b3d03730ad7a5c31dc8475fa
adityadevraj699 <aditya.kumar1.cs.2022@mitmeerut.ac.in>
2025-10-22 16:55:41 +0530

    Refactor ProfilingWrapper to run tasks in a virtual thread for accurate I/O profiling

--- src/main/java/com/autotasks/jar/profiling/ProfilingWrapper.java
+++ src/main/java/com/autotasks/jar/profiling/ProfilingWrapper.java
@@ -1,19 +1,26 @@
 package com.autotasks.jar.profiling;
 
 import com.autotasks.jar.thread.SmartTask;
+import java.util.concurrent.Executors;
 
 public class ProfilingWrapper implements Runnable {
     private final SmartTask task;
 
-    public ProfilingWrapper(SmartTask task) { this.task = task; }
+    public ProfilingWrapper(SmartTask task) {
+        this.task = task;
+    }
 
     @Override
     public void run() {
-        task.runTask(); // actual run
+        // Run actual task in a virtual thread during profiling for accurate I/O detection
+        try (var exec = Executors.newVirtualThreadPerTaskExecutor()) {
+            exec.submit(() -> task.runTask()).get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void profileOnceAndStore() {
         TaskProfiler.profileAndStore(this, task.getTaskName());
     }
-
 }
