ff6b5e9f79d0feb7691239cf25a6be664a1c2cd9
adityadevraj699 <aditya.kumar1.cs.2022@mitmeerut.ac.in>
2025-10-22 16:59:25 +0530

    Remove virtual thread execution from ProfilingWrapper for direct task running

--- src/main/java/com/autotasks/jar/profiling/ProfilingWrapper.java
+++ src/main/java/com/autotasks/jar/profiling/ProfilingWrapper.java
@@ -12,13 +12,9 @@ public class ProfilingWrapper implements Runnable {
 
     @Override
     public void run() {
-        // Run actual task in a virtual thread during profiling for accurate I/O detection
-        try (var exec = Executors.newVirtualThreadPerTaskExecutor()) {
-            exec.submit(() -> task.runTask()).get();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        task.runTask(); // run directly
     }
+
 
     public void profileOnceAndStore() {
         TaskProfiler.profileAndStore(this, task.getTaskName());
